ed8fb9cb777e96aa76539ae836f778c5792d7641
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2020-04-21 14:42:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setPresence({ game: { name: 'Dope Gaming', type: 2}, status: 'online'}).then(console.log).catch(console.error);
+    client.user.setStatus('online').then(console.log).catch(console.error);
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+});
+
+client.login(process.env.BOT_TOKEN);
